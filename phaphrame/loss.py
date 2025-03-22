73a2b67faeca61a5a202fa5ccf8189c8ecd909fe
Pha <anhphamhoang04@gmail.com>
2025-03-22 11:35:06 +0700

    add BCE and CE

--- phaphrame/loss.py
+++ phaphrame/loss.py
@@ -1,9 +1,14 @@
 import numpy as np
 
 class Loss:
-    def __init__(self, label, prediction):
-        self.label = label
-        self.prediction = prediction
+    def __init__(self):
+        pass
 
-    def MSEloss(self):
-        return np.mean((self.label - self.prediction) ** 2)
+    def MSELoss(self, label, prediction):
+        return np.mean((label - prediction) ** 2)
+
+    def BCELoss(self, label, prediction):
+        return -np.mean(label * np.log(prediction + 1e-8) + (1 - label) * np.log(1 - prediction + 1e-8))
+
+    def CELoss(self, label, prediction):
+        return -np.mean(label * np.log(prediction + 1e-8))
